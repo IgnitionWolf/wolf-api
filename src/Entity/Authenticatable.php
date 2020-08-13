03b13744e0af8d03d3cd82efea24760ff04f7563
mauriciovillam <mecaurio11@gmail.com>
2020-08-13 18:49:05 -0300

    Update authenticatable

--- src/Entity/Authenticatable.php
+++ src/Entity/Authenticatable.php
@@ -6,12 +6,15 @@ use IgnitionWolf\API\Entity\Automap\Automapable;
 use IgnitionWolf\API\Traits\HasRelationships;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User;
+use Silber\Bouncer\Database\HasRolesAndAbilities;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Authenticatable extends User
+class Authenticatable extends User implements JWTSubject
 {
     use Automapable;
     use SoftDeletes;
     use HasRelationships;
+    use HasRolesAndAbilities;
 
     /**
      * Automapable settings.
@@ -19,4 +22,24 @@ class Authenticatable extends User
      * @var array
      */
     protected $map = [];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
